f9e52de52ac9f36a0eb1a25287b8741d963fc34c
Ruben Undheim <ruben.undheim@gmail.com>
2014-09-07 14:14:59 +0000

    Removed

--- createdeb.sh
+++ createdeb.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 PACKAGENAME=qrouter
-VERSION=1.1.54
+VERSION=1.1.55
 rm -rf createdeb/
 mkdir createdeb
-#rm -rf createdeb/$PACKAGENAME-$VERSION
-mkdir createdeb/$PACKAGENAME-$VERSION
-git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
+rm -rf createdeb/$PACKAGENAME-$VERSION
+#mkdir createdeb/$PACKAGENAME-$VERSION
+#git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
+wget http://opencircuitdesign.com/qrouter/archive/qrouter-1.1.55.tgz
+tar -xvf qrouter-1.1.55.tgz
 tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
 #tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
 cp -r -v ../debian $PACKAGENAME-$VERSION/
