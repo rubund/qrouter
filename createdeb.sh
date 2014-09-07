86e816ba5cb60c21a3ccc18f17143bcfc65b37d7
Ruben Undheim <ruben.undheim@gmail.com>
2014-09-07 14:17:32 +0000

    Using 1.2.29 instead

--- createdeb.sh
+++ createdeb.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 PACKAGENAME=qrouter
-VERSION=1.1.55
+VERSION=1.2.29
 rm -rf createdeb/
 mkdir createdeb
 rm -rf createdeb/$PACKAGENAME-$VERSION
 #mkdir createdeb/$PACKAGENAME-$VERSION
 #git archive master | tar -x -C createdeb/$PACKAGENAME-$VERSION
 cd createdeb
-wget http://opencircuitdesign.com/qrouter/archive/qrouter-1.1.55.tgz
-tar -xvf qrouter-1.1.55.tgz
+wget http://opencircuitdesign.com/qrouter/archive/qrouter-1.2.29.tgz
+tar -xvf qrouter-1.2.29.tgz
 tar -czvf $PACKAGENAME\_$VERSION.orig.tar.gz $PACKAGENAME-$VERSION
 #tar -xvf $PACKAGENAME\_$VERSION.orig.tar.gz
 cp -r -v ../debian $PACKAGENAME-$VERSION/
